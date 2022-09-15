0dec2a7dbd996d9245141ddaf9770330dd8c26fb
the-abe-train <abraham.train@gmail.com>
2022-09-14 23:06:00 -0400

    Turned into PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
-import solidSvg from "vite-plugin-solid-svg";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  // plugins: [solidPlugin(), solidSvg({ defaultExport: "component" })],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      injectRegister: "auto",
+      strategies: "generateSW",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Motzash",
+        short_name: "Motzash",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#FFFF66",
+        background_color: "#FCFCCF",
+        display: "standalone",
+        start_url: ".",
+        description:
+          "Motzash is your dashboard companion for Jewish holidays. Use Motzash to get time for candle lighting and havdalah for Shabbat and Chag, and plan your Holy days with friends!",
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
