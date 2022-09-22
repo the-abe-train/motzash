730bc68e3762262fcbe0399b6a73112a06ebc694
the-abe-train <abraham.train@gmail.com>
2022-09-22 17:10:55 -0400

    Update to auth

--- src/context/auth2.tsx
+++ src/context/auth2.tsx
@@ -14,15 +14,23 @@ export const AuthProvider: ContextProviderComponent<User | null> = (props) => {
 
   let listener: Subscription | null;
 
-  listener = supabase.auth.onAuthStateChange(async (event, session) => {
-    console.log("Auth state change");
-    console.log(event);
-    setUser(session?.user || { id: "" });
-  }).subscription;
-
   onMount(async () => {
     const newUser = await supabase.auth.getUser();
     setUser(newUser.data.user || { id: "" });
+    listener = supabase.auth.onAuthStateChange(async (event, session) => {
+      console.log("Auth state change");
+      console.log(event);
+      // setUser(session?.user || { id: "" });
+      if (event === "SIGNED_IN" && session) {
+        setUser(session.user);
+        // SupabaseAuthService.user.next(session.user); // if using observables
+      } else if (session === null) {
+        setUser({});
+        // SupabaseAuthService.user.next(null); // if using observables
+      }
+      window.location.reload();
+      // this.loadProfile(); // etc.
+    }).subscription;
   });
 
   createEffect(() => {
